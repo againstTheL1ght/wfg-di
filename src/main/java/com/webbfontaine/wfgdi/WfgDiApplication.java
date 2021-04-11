040860f22797f313f43545f77aa768a7d0383cee
jabiog <john.abiog@webbfontaine.com>
2021-04-11 20:52:26 +0800

    initial commit

---
+++ src/main/java/com/webbfontaine/wfgdi/WfgDiApplication.java
@@ -0,0 +1,13 @@
+package com.webbfontaine.wfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfgDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfgDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webbfontaine/wfgdi/WfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbfontaine.wfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
